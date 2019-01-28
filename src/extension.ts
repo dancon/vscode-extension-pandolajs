5cb1e17128ab16a5fd2b7082639806aa54dff983
sizhao <870301137@qq.com>
2019-01-28 20:54:37 +0800

    feat: learn webview lifecircle

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,48 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('catCoding.start', () => {
 		// 创建一个 webview
-		vscode.window.createWebviewPanel(
+		const panel = vscode.window.createWebviewPanel(
 			'catCoding',
 			'Cat Coding',
 			vscode.ViewColumn.One,
 			{}
 		);
+
+		// 设置 panel 的内容
+		panel.webview.html = getWebviewContent('https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif');
+
+		const timeId = setTimeout(() => {
+			panel.dispose();
+		}, 5000);
+
+		panel.onDidDispose(
+			() => {
+				console.log('Webview 销毁了');
+				clearTimeout(timeId);
+			},
+			null,
+			context.subscriptions
+		);
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function getWebviewContent (cat: string) {
+	return `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<h1>Cat Coding</h1>
+				<img src="${cat}" width="300" />
+			</body>
+		</html>
+	`;
 }
 
 // this method is called when your extension is deactivated
